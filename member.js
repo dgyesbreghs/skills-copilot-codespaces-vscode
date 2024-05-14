c8018ee465668be56387a8413491e2dbeb2c922e
Dylan Gyesbreghs <dgyesbreghs@gmail.com>
2024-05-14 17:34:46 +0000

    Second copilot commit

---
+++ member.js
@@ -0,0 +1,11 @@
+
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 25,
+        skills: ['Javascript', 'HTML', 'CSS'],
+        calculate: calculateNumbers
+    };
+
+    return member;
+}
